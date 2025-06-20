c618ccecbed6157da32c23447831b453296e3758
ubska <alessandraubska@gmail.com>
2025-06-20 14:16:29 +0200

    feat(model): add fillable in client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'phone_number',
+        'email',
+        'address'
+    ];
 }
