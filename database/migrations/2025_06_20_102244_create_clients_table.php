ccd890390edad2afd759070f3bcb3ce2c8af4e48
ubska <alessandraubska@gmail.com>
2025-06-20 13:07:43 +0200

    feat(migrations): create clients table

--- database/migrations/2025_06_20_102244_create_clients_table.php
+++ database/migrations/2025_06_20_102244_create_clients_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number')->nullable();
+            $table->string('email')->unique();
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
