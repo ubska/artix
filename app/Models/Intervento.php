52ab75861509d2bb1b5de28ae61b92eceea03dc8
ubska <alessandraubska@gmail.com>
2025-06-25 13:59:24 +0200

    feat: create Intervento model

---
+++ app/Models/Intervento.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Intervento extends Model
+{
+    protected $fillable = [
+        'client_id',
+        'descrizione',
+        'data_intervento',
+        'note',
+    ];
+}
